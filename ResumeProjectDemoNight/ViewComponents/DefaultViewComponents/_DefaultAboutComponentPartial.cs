c4222cc1d11afafa5aa6b4f1f82bc503e1bd47d7
MehmetTufan052 <tufaneser8@gmail.com>
2026-01-08 22:13:23 +0300

    update

---
+++ ResumeProjectDemoNight/ViewComponents/DefaultViewComponents/_DefaultAboutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ResumeProjectDemoNight.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultAboutComponentPartial : ViewComponent
+    {   
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    
+    }
+}
---
+++ ResumeProjectDemoNight/ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProjectDemoNight.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
